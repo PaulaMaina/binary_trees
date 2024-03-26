5ab87d956a0625ed2e4256dd416decb2706d493b
PaulaMaina <kabura.mainap@gmail.com>
2024-03-26 17:37:22 +0300

    Preorder traversal

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -14,7 +14,7 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 	if (tree && func)
 	{
 		func(tree->n);
-		binary_tree_preorder(left, func);
-		binary_tree_preorder(right, func);
+		binary_tree_preorder(tree->left, func);
+		binary_tree_preorder(tree->right, func);
 	}
 }
