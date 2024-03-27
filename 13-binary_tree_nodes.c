493d1d0f591eb2344b2f2982e3daa9d8b7d08dfe
PaulaMaina <kabura.mainap@gmail.com>
2024-03-27 14:36:13 +0300

    Nodes with at least 1 child

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_nodes - Counts the nodes with at least one child in a binary tree
+ * binary_tree_nodes - Counts the nodes with at least 1 child in a binary tree
  * @tree: Pointer to the root node of the tree
  *
  * Description: If tree is NULL, the function must return 0
